8ed8d301dba19dd3c535b3a80e8895896018c2cd
bhim subedi <lovelotusist@hotmail.com>
2013-11-12 16:02:43 -0700

    new look3

--- nmhidta/app/models/database.rb
+++ nmhidta/app/models/database.rb
@@ -1,3 +1,5 @@
 class Database < ActiveRecord::Base
-  attr_accessible :address, :city, :destination, :dob, :fbin, :first_name, :last_name, :make, :mile_marker, :model, :origin, :other1, :other2, :other3, :other4, :owner, :picture, :registration, :road, :ssn, :state, :year
+  attr_accessible :address, :city, :country, :destination, :dob, :fbin, :first_name, :last_name, :make, :mile_marker, :model, :origin, :other1, :other2, :other3, :other4, :owner, :picture, :registration, :road, :ssn, :state, :year 
 end
+#  :seizing_taskforce, :investigating_agency, :agency_case, :cjis, :hidta_initiative
+ # 				  :date_of_seizure, :time, :day_of_week, :type_of_event
